29692ca7326de5fbce7133a6d3ce11a7a555b960
Star <mail.starcreator@gmail.com>
2021-05-24 18:19:40 +0100

    use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,56 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from "../components/SearchBox";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      asanas: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [asanas, setAsanas] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://star-creator.github.io/yoga/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((users) => {
-        this.setState({ asanas: users });
-      });
-  }
+      .then((response) => {return response.json();})
+      .then((users) => {setAsanas(users);});
+  },[]);
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    const { asanas, searchfield } = this.state;
+  const filteredAsanas = asanas.filter((asana) => {
+    const english = asana.english_name.toLowerCase().includes(searchfield.toLowerCase()),
+        sanskrit = asana.sanskrit_name.toLowerCase().includes(searchfield.toLowerCase());
 
-    const filteredAsanas = asanas.filter((asana) => {
-      const english = asana.english_name.toLowerCase().includes(searchfield.toLowerCase()),
-          sanskrit = asana.sanskrit_name.toLowerCase().includes(searchfield.toLowerCase());
+    return english || sanskrit
+  });
 
-      return english || sanskrit
-    });
-
-    return !asanas.length ? (
-      <h1 className="tc">Loading ...</h1>
-    ) : (
-      <div className="tc">
-        <h1>Yoga Asanas</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundry>
-            <CardList asanas={filteredAsanas} />
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    );
-  }
+  return !asanas.length ? (
+    <h1 className="tc">Loading ...</h1>
+  ) : (
+    <div className="tc">
+      <h1>Yoga Asanas</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList asanas={filteredAsanas} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
